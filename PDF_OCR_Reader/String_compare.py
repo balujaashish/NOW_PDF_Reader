270c52febac10a000eebe3e1cab82e653da2da0d
unknown <ashish.baluja@boardwalktech.com>
2020-03-30 14:29:57 -0700

    added comments to string compare

--- PDF_OCR_Reader/String_compare.py
+++ PDF_OCR_Reader/String_compare.py
@@ -6,6 +6,21 @@ class String_Compare():
         pass
 
     def str_compare(self, str1, str2):
+        """
+        compare string and return an enum:
+            => 1: when two strings are equal
+            => 2: when str1 > str2
+            => 3: when str1 < str2
+            => 0: when they are not string and cant be compared.
+            it takes care of numbers and special char too as long as they are in string form. 
+
+        Args:
+            str1 (string): string to be compared.
+            str2 (string): string to be compared with.
+            
+        Returns:
+            enum.
+        """
         try:
             if str1 == str2: return 1
             elif str1 > str2: return 2
