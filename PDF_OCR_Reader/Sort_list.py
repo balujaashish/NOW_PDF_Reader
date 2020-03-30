d109aa883925481347b33cc92640e4e5a368d99e
unknown <ashish.baluja@boardwalktech.com>
2020-03-30 13:44:19 -0700

    2D list sort

---
+++ PDF_OCR_Reader/Sort_list.py
@@ -0,0 +1,23 @@
+from operator import itemgetter
+
+
+class sort_List():
+    def __init__(self):
+        pass
+
+        
+    def sort_2D_list(self, p_list, p_position):
+        """
+        sort a list, tuple that is 2 dimentional, thus we need a position using which we will sort the list.
+
+        Args:
+            p_list (list/tuple): list that needs to be sorted.
+            p_position (int): position at which the value sits using which we will sort the array.
+            
+        Returns:
+            sorted List or tuple
+        """
+        return sorted(p_list,key=itemgetter(p_position-1))
+
+    
+    
---
+++ PDF_OCR_Reader/test_Sort_list.py
@@ -0,0 +1,33 @@
+import unittest
+from Sort_list import sort_List
+
+class testSortList(unittest.TestCase):
+
+    def setUp(self):
+        self.x = sort_List()
+    
+    def test_sort_2D_list(self):
+        l_list = [[1,1,'a'], [1,1,'d'], [1,1,'c']]
+        l_position = 3
+        self.assertEqual(self.x.sort_2D_list(l_list,l_position),[[1,1,'a'], [1,1,'c'], [1,1,'d']])
+        
+        l_list = [[1,1,'a'], [1,1,'2'], [1,1,'c']]
+        l_position = 3
+        y = self.x.sort_2D_list(l_list,l_position)
+        self.assertEqual(y,[[1,1,'2'], [1,1,'a'], [1,1,'c']])
+        self.assertGreaterEqual(y[2][l_position-1],y[1][l_position-1])
+
+        l_list = [[1,3,'fwf'], [2,3,'hd'], [3,3,'iosd'], [2,83,'kFf'], [93,3,'sndsfkmsS'], [4,3,'YSDVG'],[5,3,'kFf'], [6,3,'dffv'],[2,3,'8834']]
+        l_position = 3
+        y = self.x.sort_2D_list(l_list,l_position)
+        i = 0
+        len_y = len(y)
+        while i < len_y-1:
+            self.assertGreaterEqual(y[i+1][l_position-1],y[i][l_position-1])
+            i = i+1
+        
+
+
+        
+if __name__ == "__main__":
+    unittest.main()
