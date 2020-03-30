556f6e1680c0f337b651ee8bfcbab7e80127e0d9
unknown <ashish.baluja@boardwalktech.com>
2020-03-30 14:22:54 -0700

    updated text

--- PDF_OCR_Reader/Sort_list.py
+++ PDF_OCR_Reader/Sort_list.py
@@ -8,7 +8,7 @@ class sort_List():
         
     def sort_2D_list(self, p_list, p_position):
         """
-        sort a list, tuple that is 2 dimentional, thus we need a position using which we will sort the list.
+        sort a list or tuple that is 2 dimentional, thus we need a position using which we will sort the list.
 
         Args:
             p_list (list/tuple): list that needs to be sorted.
